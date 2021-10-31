fb3d73325d25033ad9e1eb134c1808c0d24e520e
mtedeschini <matiastedeschini@gmail.com>
2021-10-31 20:24:50 -0300

    Cambios

--- src/App.js
+++ src/App.js
@@ -2,42 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 import Pelicula from './Pelicula';
 import PageWrapper from './PageWrapper';
-import peliculasJson from './peliculas.json';
+//import peliculasJson from './peliculas.json';
 import Paginacion from './Paginacion';
 import { useState } from 'react';
 
 function App() {
 
   const [paginaActual, setPaginaActual] = useState (1);
+  const [peliculas, setPeliculas] = useState ([]);
   const [cantidadActual, setCantidadActual] = useState (3);
   var totalPorPagina = cantidadActual;
-  let peliculas = peliculasJson; //Importo json con info películas
+  //let peliculas = peliculasJson; //Importo json con info películas
   let peliculasPorPagina = [];
 
-  const buscarPelicula = async () => {
-    let url = "https://cors-anywhere.herokuapp.com/https://lucasmoy.dev/data/react/peliculas.json";
-
+  const buscarPeliculas = async () => {
+    let url = "http://lucasmoy.dev/data/react/peliculas.json";
+    
     let respuesta = await fetch(url, {
         "method" : "GET",
-        "mode": "no-cors",
         "headers": {
           "Accept": 'application/json',
-          "Content-Type" : 'application/json',
-          "Origin": 'ttps://lucasmoy.dev'
+          "Content-Type" : 'application/json' 
         }
     });
-    let json = await respuesta.json();
-    alert(json);
+    let json = await respuesta.json(); 
+    setPeliculas(json);
   }
-
-  buscarPelicula();
 
   const cargarPeliculas = () => {
     //Si estoy en pagina 1, primera pagina va a mostrar la pelicula 0 a 4.
     // peliculas.slice(0, 5);
     // peliculas.slice(5, 10);
-    peliculasPorPagina = peliculas.slice((paginaActual - 1 ) * totalPorPagina, paginaActual * totalPorPagina )
-    }
+/*     peliculasPorPagina = peliculas.slice((paginaActual - 1 ) * totalPorPagina, paginaActual * totalPorPagina )
+ */    }
   
   const getTotalPaginas = () =>{
     let cantidadPeliculas = peliculas.length;
@@ -51,6 +48,7 @@ function App() {
     <PageWrapper 
     cantidadPeliculas = {peliculas.length}>
       
+      <button onClick={buscarPeliculas}> PRUEBA </button>
       {peliculasPorPagina.map(pelicula => {
         return(
           <Pelicula 
