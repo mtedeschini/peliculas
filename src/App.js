f64b2c25148168c51e295917d9cd6402c8722dae
mtedeschini <matiastedeschini@gmail.com>
2021-10-31 15:33:28 -0300

    Cambios

--- src/App.js
+++ src/App.js
@@ -4,15 +4,46 @@ import Pelicula from './Pelicula';
 import PageWrapper from './PageWrapper';
 import peliculasJson from './peliculas.json';
 import Paginacion from './Paginacion';
+import { useState } from 'react';
 
 function App() {
 
+  const [paginaActual, setPaginaActual] = useState (1);
+  const [cantidadActual, setCantidadActual] = useState (3);
+  var totalPorPagina = cantidadActual;
   let peliculas = peliculasJson; //Importo json con info películas
+  let peliculasPorPagina = [];
 
+  const buscarPelicula = () => {
+    let url = "https://lucasmoy.dev/data/react/peliculas.json";
+    fetch (url, {
+      "method" : "GET",
+      "headers": {
+        "Accept": 'application/json',
+        "Content-Type" : 'application/json'
+      }
+    })
+  }
+
+  const cargarPeliculas = () => {
+    //Si estoy en pagina 1, primera pagina va a mostrar la pelicula 0 a 4.
+    // peliculas.slice(0, 5);
+    // peliculas.slice(5, 10);
+    peliculasPorPagina = peliculas.slice((paginaActual - 1 ) * totalPorPagina, paginaActual * totalPorPagina )
+    }
+  
+  const getTotalPaginas = () =>{
+    let cantidadPeliculas = peliculas.length;
+    return Math.ceil(cantidadPeliculas / totalPorPagina);
+  }
+  
+  cargarPeliculas();
+  
   return (
-    <PageWrapper>
+    <PageWrapper 
+    cantidadPeliculas = {peliculas.length}>
       
-      {peliculas.map(pelicula => {
+      {peliculasPorPagina.map(pelicula => {
         return(
           <Pelicula 
             titulo={pelicula.titulo} 
@@ -24,13 +55,21 @@ function App() {
             mmpa= {pelicula.mmpa} 
             release= {pelicula.release} 
             director= {pelicula.director} 
-            actor= {pelicula.actores} 
+            actores= {pelicula.actores} 
           />
           )})}
 
-      <Paginacion pagina={2} total={4} onChange={(pagina) => {
-        alert(pagina)
-      }}/>
+      <Paginacion 
+      pagina={paginaActual} 
+      total={getTotalPaginas()} 
+      cambiarPagina={(pagina) => {
+         setPaginaActual(pagina);
+      }}
+      cantidad={cantidadActual}
+      cambiarCantidad={(cantidad) => {
+        setCantidadActual(cantidad);
+      }}
+      />
 
     </PageWrapper>
   );
--- src/Paginacion.js
+++ src/Paginacion.js
@@ -1,19 +1,49 @@
 export default function Paginacion(props) {
+    
+    const getPaginas = () => {
+        const resultado = [];
+        for (let i = 0; i < props.total ; i++) {
+            let pagina = i + 1;
+            resultado.push(
+                <a onClick={() => props.cambiarPagina(pagina)} 
+                className={props.pagina === pagina  ? 'active' : ''} 
+                >
+                {pagina}
+                </a>
+            )}
+        return resultado;
+    }
 
-    {props.pagina}
-    {props.total}
-    {props.onChange}
-
+    const getCantidadActual = () => {
+        const cantidadPorPagina = [];
+        for (let i = 0; i < 3 ; i++) {
+            let cantidad = (i+1) * 3;
+            cantidadPorPagina.push(
+                <a onClick={() => props.cambiarCantidad(cantidad)} 
+                className={props.cantidad === cantidad  ? 'active' : ''} 
+                >
+                {cantidad}
+                </a>
+            )}
+        return cantidadPorPagina; 
+    }
+    
     return (
+        
         <div className="topbar-filter">
             <div className="pagination2">
                 <span>Página {props.pagina} de {props.total}</span>
-
-
-                    <a className="active" href="#">1</a>
                 
-                <a href="#">2</a>
-                <a href="#"><i className="ion-arrow-right-b"></i></a>
+                {getPaginas()}
+
+                <a><i className="ion-arrow-right-b"></i></a>
+  
+            </div>
+            <div className="pagination2">
+                <span>Películas por página</span>       
+
+                {getCantidadActual()}
+  
             </div>
         </div>
     );
